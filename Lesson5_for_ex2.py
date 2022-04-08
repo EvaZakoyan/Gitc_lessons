87b6761a49fb2b12003ff6a9fc4cea54dc346d45
EvaZakoyan <102632890+EvaZakoyan@users.noreply.github.com>
2022-04-09 02:22:22 +0400

    Add files via upload

---
+++ Lesson5_for_ex2.py
@@ -0,0 +1,7 @@
+#input two two-digit natural .The program must find the smallest and largest digit of each.
+
+num_1 = input('Input two-digit number: ')
+num_2 = input('Input two-digit number: ')
+
+print( f'The biggest :{max(num_1)}, the smallest :{min(num_1)}')
+print( f'The biggest :{max(num_2)}, the smallest :{min(num_2)}')
