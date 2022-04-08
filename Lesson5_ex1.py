387875b54ec4442fd762853833c61d41c0f70b20
EvaZakoyan <102632890+EvaZakoyan@users.noreply.github.com>
2022-04-09 00:48:09 +0400

    Delete Lesson5_ex1.py

--- Lesson5_ex1.py
+++
@@ -1,9 +0,0 @@
-# Convert the number entered by the user to bytes or kilobytes, choised by user
-
-a = float(input("input any number"))
-cbs = input('Choise you want to convert to "b to kb" or "kb to b"')
-
-if cbs == "b to kb":
-    print(f" {a} bytes =  {a /1024} kilobytes")
-if cbs == "kb to b":
-    print(f" {a} kilobytes = {a*1024} bytes ")
